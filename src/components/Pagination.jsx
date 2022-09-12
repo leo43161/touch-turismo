6dea3127f0104fa00e9910f02535412ca824b1a3
Leonardo Palavecino <64588981+leo43161@users.noreply.github.com>
2022-09-12 12:59:45 -0300

    Se agrego la logica para poder recortar las paginas dibujadas y se deben aplicar

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,6 +1,34 @@
 import Pagination from 'react-bootstrap/Pagination';
 
-export default function PaginationTouch({ perPages, total, paginate, page, type }) {
+export default function PaginationTouch({ perPages, total, paginate, type }) {
+    /*  */
+    const lastPage = Math.ceil(total / perPages);
+    console.log(lastPage)
+    console.log("lastPage")
+    const page = 41;
+    console.log(total);
+    const delta = 2;
+    const range = [];
+
+    for (let i = Math.max(2, (page - delta)); i <= Math.min((lastPage - 1), (page + delta)); i += 1) {
+        range.push(i);
+    }
+
+    if ((page - delta) > 2) {
+        range.unshift('...');
+    }
+
+    if ((page + delta) < (lastPage - 1)) {
+        range.push('...');
+    }
+
+    range.unshift(1);
+    if (lastPage !== 1) range.push(lastPage);
+
+    console.log(range);
+
+    /*  */
+
     const pageNumbers = [];
 
     for (let i = 1; i <= Math.ceil(total / perPages); i++) {
