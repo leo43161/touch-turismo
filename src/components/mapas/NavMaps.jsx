7958fe02a7d739777a80c211c30adde2a39c5524
Leonardo Palavecino <64588981+leo43161@users.noreply.github.com>
2022-09-20 10:53:21 -0300

    Se agregaron los otros mapas y ademas lo qr faltantes

--- src/components/mapas/NavMaps.jsx
+++ src/components/mapas/NavMaps.jsx
@@ -7,16 +7,16 @@ export default function NavMaps() {
             <div className="col p-4 d-flex align-items-center" onClick={() => router.push('/mapas/ciudadhistorica')} style={{ backgroundColor: "#0088BA" }}>
                 <h2 className="mb-0">CIUDAD HISTORICA</h2>
             </div>
-            <div className="col p-4 d-flex align-items-center" onClick={() => router.push('/mapas/destino')} style={{ backgroundColor: "#99B932" }}>
+            <div className="col p-4 d-flex align-items-center" onClick={() => router.push('/mapas/yungas')} style={{ backgroundColor: "#99B932" }}>
                 <h2 className="mb-0">YUNGAS</h2>
             </div>
-            <div className="col p-4 d-flex align-items-center" onClick={() => router.push('/mapas/destino')} style={{ backgroundColor: "#BA007D" }}>
+            <div className="col p-4 d-flex align-items-center" onClick={() => router.push('/mapas/valles')} style={{ backgroundColor: "#BA007D" }}>
                 <h2 className="mb-0">VALLES CALCHAQUIES</h2>
             </div>
-            <div className="col p-4 d-flex align-items-center" onClick={() => router.push('/mapas/destino')} style={{ backgroundColor: "#DD9A0B" }}>
+            <div className="col p-4 d-flex align-items-center" onClick={() => router.push('/mapas/choromoro')} style={{ backgroundColor: "#DD9A0B" }}>
                 <h2 className="mb-0">VALLE DE CHOROMOR</h2>
             </div>
-            <div className="col p-4 d-flex align-items-center" onClick={() => router.push('/mapas/destino')} style={{ backgroundColor: "#613980" }}>
+            <div className="col p-4 d-flex align-items-center" onClick={() => router.push('/mapas/sur')} style={{ backgroundColor: "#613980" }}>
                 <h2 className="mb-0">SUR</h2>
             </div>
         </div>
--- src/pages/mapas/yungas.jsx
+++ src/pages/mapas/yungas.jsx
@@ -1,22 +1,22 @@
 import HeaderSecc from "../../components/HeaderSecc"
 import NavMaps from "../../components/mapas/NavMaps";
 
-export default function ciudadhistorica() {
+export default function yungas() {
     return (
         <div style={{ backgroundColor: "#ABA693" }}>
             <HeaderSecc title="Mapas" icon="map" color="#E51E21"></HeaderSecc>
             <div className="text-center pt-3 text-black d-flex px-4">
-                <div className="py-2 px-3" style={{ backgroundColor: "#0088BA" }}>
-                    <h2 className="mb-0 text-white">CIRCUITO CIUDAD HISTORICA</h2>
+                <div className="py-2 px-3" style={{ backgroundColor: "#99B932" }}>
+                    <h2 className="mb-0 text-white">CIRCUITO YUNGAS</h2>
                 </div>
             </div>
             <div>
                 <div className="position-relative px-4 pt-3">
-                    <img src="/img/mapas/mapas_historica.png" className="img-fluid" alt="" />
+                    <img src="/img/mapas/mapas_yungas.png" className="img-fluid" alt="" />
                 </div>
                 <div className="d-flex justify-content-end mt-3">
                     <div className="col-3 mb-3">
-                        <img className="img-fluid col-11" src="/img/mapas/qr_historica.png" alt="" />
+                        <img className="img-fluid col-11" src="/img/mapas/qr_yungas.png" alt="" />
                     </div>
                 </div>
                 <div className="text-center py-3 text-black border-top">
