8836bdeac3bacabeef3deeb381fa557bc1c8b3d9
Leonardo Palavecino <64588981+leo43161@users.noreply.github.com>
2022-11-03 11:37:28 -0300

    Se agregaron los datos de los alquuileres de auto y se empezaron a visualizar

--- src/components/transportes/ModalAutos.jsx
+++ src/components/transportes/ModalAutos.jsx
@@ -1,6 +1,7 @@
 import Modal from 'react-bootstrap/Modal';
 import { FaCarAlt, FaPhoneAlt, FaEnvelope } from 'react-icons/fa';
 import Button from 'react-bootstrap/Button';
+import Autos from '../../data/Autos';
 
 export default function ModalAutos({ show, handleClose }) {
     return (
@@ -11,28 +12,31 @@ export default function ModalAutos({ show, handleClose }) {
             <Modal.Body>
                 <div>
                     <div>
-                        <div className="card mb-3 px-4 shadow-sm">
-                            <div className="d-flex align-items-center">
-                                <div>
-                                    <FaCarAlt className='text-trans icon-size-3'></FaCarAlt>
-                                </div>
-                                <div className="card-body px-3">
-                                    <h2 className="text-center mb-1 d-flex align-items-center">Avis</h2>
-                                    <div className="d-flex align-items-center mb-2">
-                                        <FaPhoneAlt className="fs-4 me-1 secondary-text"></FaPhoneAlt>
-                                        <h5 className="secondary-text mb-0 ms-1">
-                                            (0381) 4310025 / (0381) 4267777 (Aerop) / (0381) 155616919 (24 hs) / 381-5069529 (Martin)
-                                        </h5>
+                        {Autos.map(({ id, titulo, number, mail }) =>
+                            <div className="card mb-3 px-4 shadow-sm" key={id}>
+                                <div className="d-flex align-items-center">
+                                    <div>
+                                        <FaCarAlt className='text-trans icon-size-3'></FaCarAlt>
                                     </div>
-                                    <div className="d-flex align-items-center">
-                                        <FaEnvelope className="fs-4 me-1 secondary-text"></FaEnvelope>
-                                        <h5 className="secondary-text mb-0 ms-1">
-                                            tucuman@avis.com.ar
-                                        </h5>
+                                    <div className="card-body px-3">
+                                        <h2 className="text-center mb-1 d-flex align-items-center">{titulo}</h2>
+                                        <div className="d-flex align-items-center mb-2">
+                                            <FaPhoneAlt className="fs-4 me-1 secondary-text"></FaPhoneAlt>
+                                            <h4 className="secondary-text mb-0 ms-1">
+                                                {number}
+                                            </h4>
+                                        </div>
+                                        {mail !== "" && <div className="d-flex align-items-center">
+                                            <FaEnvelope className="fs-4 me-1 secondary-text"></FaEnvelope>
+                                            <h5 className="secondary-text mb-0 ms-1">
+                                                {mail}
+                                            </h5>
+                                        </div>}
+
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        )}
                     </div>
                 </div>
             </Modal.Body>
---
+++ src/data/Autos.js
@@ -0,0 +1,56 @@
+export default [
+    {
+        "id": 0,
+        "titulo": "Avis",
+        "number": "(0381) 4310025 / (0381) 4267777 (Aerop) / (0381) 155616919 (24 hs) / 381-5069529 (Martin)",
+        "mail": "tucuman@avis.com.ar"
+    },
+    {
+        "id": 1,
+        "titulo": "Donde Rent a Car",
+        "number": "0381) 4281297 / (0381)155446677 (24 hs)",
+        "mail": ""
+    },
+    {
+        "id": 2,
+        "titulo": "Hertz",
+        "number": "(0381) 4524991",
+        "mail": "Tucumanctro@hertz.com.ar"
+    },
+    {
+        "id": 3,
+        "titulo": "Móvil Renta",
+        "number": "(0381) 4310550 / ( 0381)155873049 (24 hs)",
+        "mail": "informes@movilrenta.com.ar"
+    },
+    {
+        "id": 4,
+        "titulo": "Alamo Rent a Car",
+        "number": "(0381 )4228804 / (0381) 4511035 / (0381) 155037031/30",
+        "mail": "tucuman@alamoargentina.com.ar"
+    },
+    {
+        "id": 5,
+        "titulo": "Trans Class",
+        "number": "(0381) 4245030",
+        "mail": "transclass@tucbbs.com.ar"
+    },
+    {
+        "id": 6,
+        "titulo": "Localiza",
+        "number": "(0381) 4287873",
+        "mail": "reservasarg@localiza.com"
+    },
+    {
+        "id": 7,
+        "titulo": "Cactus",
+        "number": "(0387) 4693963 (24 hs) / 381 5976884 / 08107772228 (N° de reservas)",
+        "mail": "Reservas@cactus.com.ar"
+    },
+    {
+        "id": 8,
+        "titulo": "Sinergia renta car",
+        "number": "381 4735377 (8 a 22. Lun a lun) / 3814571012",
+        "mail": ""
+    }
+]
